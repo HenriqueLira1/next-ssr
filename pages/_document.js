1862898de240574fa015c78f247a2bd489e9b925
Henrique Lira <henrique.lira@18digital.com.br>
2019-12-10 14:39:15 -0300

    Add global document file

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <html>
+                <Head>
+                    <style>{`body{background: #069}`}</style>
+                </Head>
+                <Main />
+                <NextScript />
+            </html>
+        );
+    }
+}
